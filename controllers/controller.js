64fa7d130e9d7e929ec63d913e98e2321a113b12
bevrivling <bev.rivling@gmail.com>
2018-11-02 12:54:13 +0000

    basic server funcatoinality

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,22 +1,34 @@
 const http = require("http");
 const fs = require("fs");
 
-const bodBuilder = (req, cb) => {
-  let bod = "";
-  req.on("data", d => {
-    bod += d;
-  });
-  req.on("end", () => {
-    cb(null, JSON.parse(bod));
-  });
-  req.on("error", () => {
-    console.error("error");
+// const bodBuilder = (req, cb) => {
+//   let bod = "";
+//   req.on("data", d => {
+//     bod += d;
+//   });
+//   req.on("end", () => {
+//     cb(null, JSON.parse(bod));
+//   });
+//   req.on("error", () => {
+//     console.error("error");
+//   });
+// };
+
+exports.getNorthcoders = (req, res) => {
+  fs.readFile("./northcodersRaw.txt", "utf8", (err, ncstr) => {
+    res.write(ncstr);
+    res.end();
   });
 };
 
-exports.getNorthcoders = (req, res) => {
-  console.log("hello!");
-  fs.readFile("northcodersRaw.txt", (err, res) => {
-    bodBuilder(req, (err, nc) => console.log(nc));
+exports.getSingleNorthcoder = (req, res, northcoder) => {
+  fs.readFile("./northcodersRaw.txt", "utf8", (err, ncstr) => {
+    const ncEmp = JSON.parse(ncstr).filter(nc => {
+      return nc.username === northcoder;
+    });
+
+    res.write(JSON.stringify(ncEmp));
+
+    res.end();
   });
 };
--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const http = require("http");
-const { getNorthcoders } = require("./controllers/controller.js");
+const {
+  getNorthcoders,
+  getSingleNorthcoder
+} = require("./controllers/controller.js");
 
 const server = http.createServer((request, response) => {
   const { url, method } = request;
   response.setHeader("Content-Type", "text/json");
   if (method === "GET") {
+    const ncReg = /\/api\/northcoders\/[A-z]+$/;
     if (url === "/api/northcoders") getNorthcoders(request, response);
+    if (ncReg.test(url)) {
+      const [northcoder] = url.match(/[A-z]+$/g);
+      getSingleNorthcoder(request, response, northcoder);
+    }
   }
-
-  response.end();
 });
 
 server.listen(3001);
